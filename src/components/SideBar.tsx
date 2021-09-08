24faca042a9801ec919cd7ba785f52314ed66c5f
João Marcelo Vieira <joaonininho.twt@gmail.com>
2021-09-08 15:54:04 -0300

    challenge completed

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,9 +1,10 @@
+import { List, ListRowRenderer } from "react-virtualized";
 import { Button } from "./Button";
 
 interface SideBarProps {
   genres: Array<{
     id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
     title: string;
   }>;
   selectedGenreId: number;
@@ -13,24 +14,36 @@ interface SideBarProps {
 export function SideBar({
   genres,
   selectedGenreId,
-  buttonClickCallback
+  buttonClickCallback,
 }: SideBarProps) {
+  const RowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <Button
+          title={genres[index].title}
+          iconName={genres[index].name}
+          onClick={() => buttonClickCallback(genres[index].id)}
+          selected={selectedGenreId === genres[index].id}
+        />
+      </div>
+    );
+  };
+
   return (
     <nav className="sidebar">
-      <span>Watch<p>Me</p></span>
+      <span>
+        Watch<p>Me</p>
+      </span>
 
       <div className="buttons-container">
-        {genres.map(genre => (
-          <Button
-            key={String(genre.id)}
-            title={genre.title}
-            iconName={genre.name}
-            onClick={() => buttonClickCallback(genre.id)}
-            selected={selectedGenreId === genre.id}
-          />
-        ))}
+        <List
+          rowHeight={75}
+          width={320}
+          height={451.81}
+          rowRenderer={RowRenderer}
+          rowCount={genres.length}
+        />
       </div>
-
     </nav>
-  )
+  );
 }
